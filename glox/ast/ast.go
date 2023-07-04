94a653bfcf88242ac8e2ea83fc2428112b739222
Boris Kayi <hello@silverhairs.engineer>
2023-07-04 02:05:52 +0200

    refactor: group expression node code in same file

--- glox/ast/ast.go
+++ glox/ast/ast.go
@@ -2,6 +2,8 @@ package ast
 
 import (
 	"bytes"
+	"fmt"
+	"glox/token"
 )
 
 type ExpType string
@@ -14,6 +16,12 @@ const (
 	TERNARY_EXP ExpType = "ternary"
 )
 
+type Expression interface {
+	String() string
+	Type() ExpType
+	Accept(Visitor) any
+}
+
 type Visitor interface {
 	VisitBinary(exp *Binary) any
 	VisitUnary(exp *Unary) any
@@ -22,10 +30,142 @@ type Visitor interface {
 	VisitTernary(exp *Ternary) any
 }
 
-type Expression interface {
-	String() string
-	Type() ExpType
-	Accept(Visitor) any
+type Literal struct {
+	Value any
+}
+
+func (exp *Literal) String() string {
+	return parenthesize(exp.Type(), fmt.Sprintf(" %v ", exp.Value))
+}
+
+func (exp *Literal) Type() ExpType {
+	return LITERAL_EXP
+}
+
+func NewLiteralExpression(val any) *Literal {
+	return &Literal{val}
+}
+
+func (exp *Literal) Accept(v Visitor) any {
+	return v.VisitLiteral(exp)
+}
+
+type Unary struct {
+	Operator token.Token
+	Right    Expression
+}
+
+func (exp *Unary) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("( " + exp.Operator.Lexeme + " ) ")
+	out.WriteString(exp.Right.String())
+
+	return parenthesize(exp.Type(), out.String())
+}
+
+func (exp *Unary) Type() ExpType {
+	return UNARY_EXP
+}
+
+func NewUnaryExpression(operator token.Token, right Expression) *Unary {
+	return &Unary{operator, right}
+}
+
+func (exp *Unary) Accept(v Visitor) any {
+	return v.VisitUnary(exp)
+}
+
+type Binary struct {
+	Left     Expression
+	Operator token.Token
+	Right    Expression
+}
+
+func (exp *Binary) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(exp.Left.String())
+	out.WriteString(" " + exp.Operator.Lexeme + " ")
+	out.WriteString(exp.Right.String())
+
+	return parenthesize(exp.Type(), out.String())
+}
+
+func NewBinaryExpression(left Expression, operator token.Token, right Expression) *Binary {
+	return &Binary{left, operator, right}
+}
+
+func (exp *Binary) Type() ExpType {
+	return BINARY_EXP
+}
+
+func (exp *Binary) Accept(v Visitor) any {
+	return v.VisitBinary(exp)
+}
+
+type Grouping struct {
+	Exp Expression
+}
+
+func (exp *Grouping) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("( ")
+	out.WriteString(exp.Exp.String())
+	out.WriteString(" )")
+
+	return parenthesize(exp.Type(), out.String())
+}
+
+func (exp *Grouping) Type() ExpType {
+	return GROUP_EXP
+}
+
+func NewGroupingExp(exp Expression) *Grouping {
+	return &Grouping{exp}
+}
+
+func (exp *Grouping) Accept(v Visitor) any {
+	return v.VisitGrouping(exp)
+}
+
+type Ternary struct {
+	Condition     Expression  // The expression being evaluated
+	LeftOperator  token.Token // Typicaly a `?`
+	True          Expression  // Expression executed when the condition is true
+	RightOperator token.Token // Typically `:`
+	False         Expression  // Expression executed when condition is false
+}
+
+func (exp *Ternary) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(exp.Condition.String())
+	out.WriteString(" " + exp.LeftOperator.Lexeme + " ")
+	out.WriteString("(" + exp.True.String() + ")")
+	out.WriteString(" " + exp.RightOperator.Lexeme + " ")
+	out.WriteString("(" + exp.False.String() + ")")
+
+	return parenthesize(exp.Type(), out.String())
+}
+
+func (exp *Ternary) Type() ExpType {
+	return TERNARY_EXP
+}
+
+func NewTernaryConditional(condition Expression, leftOperator token.Token, positive Expression, rightOperator token.Token, negative Expression) *Ternary {
+	return &Ternary{
+		Condition:     condition,
+		LeftOperator:  leftOperator,
+		RightOperator: rightOperator,
+		True:          positive,
+		False:         negative,
+	}
+}
+
+func (exp *Ternary) Accept(v Visitor) any {
+	return v.VisitTernary(exp)
 }
 
 func parenthesize(name ExpType, value string) string {
