9a7085414b42e29a860d83c9cbb2744b2415d3ce
Boris Kayi <hello@silverhairs.engineer>
2023-07-15 23:11:44 +0200

    refactoring: replace `Error` suffix with `Exception`

--- glox/exception/exception.go
+++ glox/exception/exception.go
@@ -6,7 +6,7 @@ import (
 
 func Generic(line int, where string, msg string) error {
 	message := fmt.Sprintf("%s @ %s", msg, where)
-	return fmt.Errorf("GloxGenericError([line %d] Error: %s)", line, message)
+	return fmt.Errorf("GloxGenericException([line %d] Error: %s)", line, message)
 }
 
 // Calls `e.New` with an empty string for the `where` argument.
@@ -14,6 +14,6 @@ func Short(line int, msg string) error {
 	return Generic(line, "", msg)
 }
 
-func RuntimeError(body any, message string) error {
-	return fmt.Errorf("RuntimeError(%+v, %s)", body, message)
+func Runtime(body any, message string) error {
+	return fmt.Errorf("RuntimeException(%+v, %s)", body, message)
 }
