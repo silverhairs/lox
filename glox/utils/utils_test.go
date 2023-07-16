6a5956eb7aeb177614c5b47308c58026a2981cc3
Boris Kayi <hello@silverhairs.engineer>
2023-07-16 06:00:50 +0200

    fix: useless test

--- glox/utils/utils_test.go
+++ glox/utils/utils_test.go
@@ -1,27 +1,30 @@
 package utils
 
 import (
-	"fmt"
 	"testing"
 )
 
+type example struct {
+	index  int
+	String string
+}
+
 func TestMap(t *testing.T) {
-	tests := []struct {
-		Value  int
-		String string
-	}{
-		{Value: 1, String: "The value is 1"},
-		{Value: 2, String: "The value is 2"},
-		{Value: 3, String: "The value is 3"},
-		{Value: 4, String: "The value is 4"},
-		{Value: 5, String: "The value is 5"},
-		{Value: 6, String: "The value is 6"},
-		{Value: 7, String: "The value is 7"},
+
+	fixtures := []example{
+		{index: 0, String: "The value is 0"},
+		{index: 1, String: "The value is 1"},
+		{index: 2, String: "The value is 2"},
+		{index: 3, String: "The value is 3"},
+		{index: 4, String: "The value is 4"},
+		{index: 5, String: "The value is 5"},
+		{index: 6, String: "The value is 6"},
 	}
 
-	for i, test := range tests {
-		expected := fmt.Sprintf("The value is %d", test.Value)
-		got := test.String
+	result := Map(fixtures, func(f example) string { return f.String })
+
+	for i, got := range result {
+		expected := fixtures[i].String
 
 		if expected != got {
 			t.Fatalf("%d failed -> expected=%q got=%q", i, expected, got)
