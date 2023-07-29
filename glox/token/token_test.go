a795a53191ba1a6d6b32152252c14238f43da712
Boris Kayi <hello@silverhairs.engineer>
2023-07-29 04:03:52 +0200

    test: add test cases for `token` pkg

---
+++ glox/token/token_test.go
@@ -0,0 +1,42 @@
+package token
+
+import "testing"
+
+func TestLookupIdentifier(t *testing.T) {
+	tests := []struct {
+		word     string
+		expected TokenType
+	}{
+		{word: "nil", expected: NIL},
+		{word: "class", expected: CLASS},
+		{word: "if", expected: IF},
+		{word: "maybe12", expected: IDENTIFIER},
+		{word: "else", expected: ELSE},
+		{word: "&&", expected: AND},
+		{word: "||", expected: OR},
+		{word: "return", expected: RETURN},
+		{word: "true", expected: TRUE},
+		{word: "false", expected: FALSE},
+		{word: "print", expected: PRINT},
+		{word: "fn", expected: FUNCTION},
+		{word: "while", expected: WHILE},
+		{word: "for", expected: FOR},
+		{word: "this", expected: THIS},
+		{word: "super", expected: SUPER},
+		{word: "let", expected: LET},
+		{word: "var", expected: LET},
+		{word: "func", expected: IDENTIFIER},
+		{word: "struct", expected: IDENTIFIER},
+		{word: "interface", expected: IDENTIFIER},
+		{word: "type", expected: IDENTIFIER},
+	}
+
+	for _, test := range tests {
+		got := LookupIdentifier(test.word)
+		expected := test.expected
+
+		if got != expected {
+			t.Fatalf("failed to get token type for '%s'. got='%v' expected='%v'", test.word, got, expected)
+		}
+	}
+}
