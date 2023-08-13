c518d5318455dca93edf7af2c2d1b90e5d1640f0
Boris Kayi <hello@silverhairs.engineer>
2023-08-13 21:08:57 +0200

    refactor: minor change

--- glox/exception/exception.go
+++ glox/exception/exception.go
@@ -5,20 +5,26 @@ import (
 	"glox/token"
 )
 
+const (
+	PARSE_EXCEPTION   = "ParseException"
+	RUNTIME_EXCEPTION = "RuntimeException"
+	GENERIC_EXCEPTION = "GenericException"
+)
+
 func Generic(line int, where string, msg string) error {
-	out := fmt.Sprintf("%s at %s", msg, where)
-	return fmt.Errorf("unhandled exception: GloxGenericException(%s)\n[line %d]", out, line)
+	out := fmt.Sprintf("%s(%s at %s)", GENERIC_EXCEPTION, msg, where)
+	return fmt.Errorf("unhandled exception: %s\n[line %d]", out, line)
 }
 
-// Calls `e.New` with an empty string for the `where` argument.
+// Calls `Generic` with an empty string for the `where` argument.
 func Short(line int, msg string) error {
 	return Generic(line, "", msg)
 }
 
 func Runtime(token token.Token, message string) error {
-	return fmt.Errorf("unhandled exception: RuntimeException(%q, %s)\n[line: %d]", token.Lexeme, message, token.Line)
+	return fmt.Errorf("unhandled exception: %s(%q, %s)\n[line: %d]", RUNTIME_EXCEPTION, token.Lexeme, message, token.Line)
 }
 
 func Parse(tok token.Token) error {
-	return fmt.Errorf("unhandled exception: ParseException(%q, illegal token)\n[line: %d]", tok.Lexeme, tok.Line)
+	return fmt.Errorf("unhandled exception: %s(%q, illegal token)\n[line: %d]", PARSE_EXCEPTION, tok.Lexeme, tok.Line)
 }
